ba664e05519b7312f8ec96675ccd5dd50137ab22
mayurwebdeveloper <mayurwebdeveloper@gmail.com>
2024-06-01 16:52:25 +0530

    Initial commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,45 @@
+<?php
+
+// app/Http/Controllers/AuthController.php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $token = $user->createToken('Personal Access Token')->plainTextToken;
+
+            return response()->json([
+                'token' => $token,
+                'user' => $user,
+            ]);
+        }
+
+        return response()->json([
+            'error' => 'Unauthorized',
+        ], 401);
+    }
+// role based 
+    public function userdetails(Request $request){
+        // $user = User::where('id',Auth::user())->get();
+        return response()->json([
+            // 'token' => $token,
+            'user' => $request->user(),
+        ]);
+
+    }
+}
---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+// database/seeders/UsersTableSeeder.php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    public function run()
+    {
+        User::create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => Hash::make('password'),
+        ]);
+    }
+}
