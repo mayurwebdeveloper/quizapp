834fdf937b658cabf215441bc57296cf29a837d9
Mayur Panchal - MBTechnoweb <mayurwebdeveloper@gmail.com>
2024-06-13 22:32:11 +0530

    Admin panel setup
    Admin panel

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-
+use App\Http\Controllers\DashboardController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -26,3 +26,4 @@ Route::group(['middleware' => ['auth', 'admin']], function () {
 });
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
